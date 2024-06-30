deea998740d061161a83e3c4225dab51fb4bd398
Nate Gichuhi <gichuhinathaniel@gmail.com>
2024-06-30 19:18:15 +0300

    simple interest calculation

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,11 +1,9 @@
 number1 = 10
-number2 =5
+number2 = 5
+sum=number1 + number2
+Subtraction=number1 -number2
+Multiplication= number1*number2
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", Subtraction)
+print("Multiplication of", number1, "and", number2, "is", Multiplication)
 
-sum = number1 + number2
-print("Addition of", number1, "and", nubmer2, "is", : sum)
-
-difference = number1 - number2
-print ("Subtruction of", number1, "and" number2, "is" : differnce)
-
-multiplication = number1 * number2
-print ("Multiplication of", number1, "and" number2, "is" : multiplication)
