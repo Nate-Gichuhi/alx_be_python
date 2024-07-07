921b5e945d5b56e373a14177ba038cf94d99a1c6
Nate Gichuhi <gichuhinathaniel@gmail.com>
2024-07-07 12:51:12 +0300

    weather advice

---
+++ weather_advice.py
@@ -0,0 +1,10 @@
+weather = int("What's the weather like today? (sunny/rainy/cold):")
+
+if (weather is "sunny"):
+    print("Wear a t-shirt and sunglasses.")
+elif (weather is "rainy"):
+    print ("Don't forget your umbrella and a raincoat.")
+elif (weather is "cold"):
+    print ("Make sure to wear a warm coat and a scarf.")
+else (weather != ("sunny, rainy, cold")):
+    print (" Sorry, I don't have recommendations for this weather.")
