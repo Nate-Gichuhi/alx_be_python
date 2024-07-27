af67553702d7e1b137650413fb17697451160632
Nate Gichuhi <gichuhinathaniel@gmail.com>
2024-07-27 22:03:29 +0300

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -11,12 +11,12 @@ class Book:
     
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        super().__init__(self, title, author)
+        super().__init__(title, author)
         self.file_size = file_size
 
 class PrintBook(Book):
       def __init__(self, title, author, page_count):
-           super().__init__(self, title, author)
+           super().__init__(title, author)
            self.page_count = page_count
           
 class Library:
