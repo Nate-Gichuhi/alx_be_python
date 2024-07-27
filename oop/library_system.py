28f69649731e638d68e4696a2f6eb01857e416a9
Nate Gichuhi <gichuhinathaniel@gmail.com>
2024-07-27 20:34:13 +0300

    library system

---
+++ oop/library_system.py
@@ -0,0 +1,36 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self): 
+        return f"Title:{self.title}"
+
+    def __str__(self):
+        return f"Author:{self.author}" 
+    
+class Ebook(Book):
+    def __init__(self, title, author, file_size):
+        super.__init__(self, title, author)
+        self.file_size = file_size
+
+class PrintBook(Book):
+      def __init__(self, title, author, page_count):
+           super.__init__(self, title, author)
+           self.page_count = page_count
+          
+class Library:
+    books = [
+        Book,
+        Ebook,
+        PrintBook
+    ]
+    def add_book(self, books):
+        self.books = books
+
+    def list_books(self):
+        print(Book(self.title, self.author))
+        print(Ebook(self.title, self.author, self.file_size))
+        print(PrintBook(self.title, self.author, self.page_count))
+
+
